c4af95bd62848120adbbf7eedcd1d50bb3ce26da
Chase Sriprajittichai <sriprajittichai@gmail.com>
2019-05-19 03:33:38 -0700

    Initial commit. Create list-user.js

---
+++ list-users.js
@@ -0,0 +1,25 @@
+var admin = require('firebase-admin');
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require('./cse110-firebase-adminsdk-private-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://cse110-6b013.firebaseio.com'
+});
+
+// Get the Auth service for the default app
+var auth = admin.auth();
+
+const listUsers = () => {
+  auth.listUsers()
+      .then((result) => result.users.forEach(user => console.log(user)))
+      .catch((error) => console.log(error))
+      .finally(() => exit());
+};
+
+const exit = () => {
+  admin.app().delete();
+}
+
+listUsers();
